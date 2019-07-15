d235fb19c8815839f9b298bcf1d2f87448e95451
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-15 16:14:05 -0400

    Configured ESlint with airbnb style guide

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      string: '',
-      array: []
+
     };
   }
+
   componentDidMount() {
-    //
   }
+
   render() {
     return (
       <div>
         <nav>
-          <div className='nav-wrapper'>
-            <div className='brand-logo'>Greenfield Logo</div>
+          <div className="nav-wrapper">
+            <div className="brand-logo">Greenfield Logo</div>
           </div>
         </nav>
       </div>
--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const path = require('path');
 
 // An instance of express
-var app = express();
+const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './client/dist')));
-
-app.get('/', (req, res) => {
-  if (err) {
-    console.log('err', err);
-    res.send(`err`).status(500);
-  } else {
-    res.send(`working`).status(200);
-  }
-});
 
 const PORT = process.env.PORT || 4000;
 
