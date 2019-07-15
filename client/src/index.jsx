806750d0c98570ecc91951dee2a0c66e7eb1c405
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-15 19:58:45 -0400

    test commit for travis

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,9 +4,11 @@ import { Provider } from 'redux';
 
 import App from './components/App';
 
+// test commit
+
 ReactDOM.render(
   // <Provider>
   <App />, // TODO: fix this
   // </Provider>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
