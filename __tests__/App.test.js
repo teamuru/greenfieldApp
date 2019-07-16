640633a7ec8f317111bbbc16f6abc1f337b88bff
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-16 14:52:05 -0400

    Implemented jest setup

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,1 +1,8 @@
-console.log('test');
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../client/src/components/App';
+
+test('It renders App', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = {
   entry: './client/src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'client', 'dist'),
     filename: 'bundle.js',
   },
   module: {
