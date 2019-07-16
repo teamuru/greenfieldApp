a87c6a0a0b628b14036f4a7970f436fa90434cce
Cali Tabilas <ctabilas23@gmail.com>
2019-07-15 22:04:11 -0400

    added some redux review boilerplate

---
+++ client/src/components/Reviews/SizeGraph.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SizeGraphs extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SizeGraphs
