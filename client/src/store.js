f90db41105d4bcdad5c1435f11a7105bfff618c7
Robert Battaglia <r083rt164@gmail.com>
2019-07-17 19:08:43 -0400

    connect questionReducer to rootReducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,19 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import {
+ createStore, combineReducers, applyMiddleware, compose 
+} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import productReducer from './reducers/productReducer';
 import reviewReducer from './reducers/reviewReducer';
+import questionsReducer from './reducers/questionsReducer';
 
 // TODO: Add other reducers to rootReducer
 const rootReducer = combineReducers({
   product: productReducer,
+  questions: questionsReducer,
   reviews: reviewReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer,
-  composeEnhancer(applyMiddleware(ReduxThunk))
-);
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(ReduxThunk)));
 export default store;
