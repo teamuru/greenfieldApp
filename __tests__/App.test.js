76f5e67add4da8bcd0ac0638e47bf2f59d6a6cd0
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-17 15:19:23 -0400

    App render test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 import App from '../client/src/components/App';
+import store from '../client/src/store';
 
 test('It renders App', () => {
-  const tree = renderer.create(<App />).toJSON();
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
