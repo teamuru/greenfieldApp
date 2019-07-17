69a11979f382f15b2571a581ce1f2e042e960f37
Hongwei <qhwatdmm@hotmail.com>
2019-07-17 14:57:02 -0400

    change name and update on bugger

---
+++ client/src/lib/calculateAverageRate.js
@@ -0,0 +1,22 @@
+// this function will take a object with k = rate; value = number on each rate
+// obj = { 2: 1, 3: 1, 4: 2, ... }
+// return average rate rounding to nearest 0.25
+
+function calculatorAverageRate(obj) {
+  let rateSum = 0;
+  let rateCounter = 0;
+  Object.keys(obj).forEach((k)=>{
+    rateSum += Number(k) * obj[k];
+      rateCounter += obj[k];
+  })
+
+  const average = rateSum / rateCounter;
+  return Number(Math.round(average * 4) / 4);
+}
+
+export default calculatorAverageRate;
+
+// test
+// var obj = {2: 1, 3: 1, 4: 2}
+// var output = calculatorAverageRate(obj)
+// console.log(output)
