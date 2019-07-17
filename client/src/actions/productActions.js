e8a4347384f778c834bea2c521e46ed61a2acd7a
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-17 16:01:22 -0400

    deleted containers and begin action

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,10 +1,6 @@
 import Axios from 'axios';
 import API_URL from '../lib/API_URL';
 
-export const fetchProductBegin = () => ({
-  type: 'FETCH_PRODUCT_BEGIN'
-});
-
 export const fetchProductSuccess = product => ({
   type: 'FETCH_PRODUCT_SUCCESS',
   payload: product
@@ -18,8 +14,6 @@ export const fetchProductFailure = error => ({
 export const fetchProduct = (prodId) => {
   const url = `https://cors-anywhere.herokuapp.com/${API_URL}/Products/${prodId}`;
   return (dispatch) => {
-    dispatch(fetchProductBegin());
-
     Axios.get(url)
       .then(({ data }) => {
         dispatch(fetchProductSuccess(data));
--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -2,10 +2,8 @@ const intialState = {};
 
 const productReducer = (state = intialState, action) => {
   switch (action.type) {
-    case 'FETCH_PRODUCT_BEGIN':
-      return { ...state, currentProductLoading: true };
     case 'FETCH_PRODUCT_SUCCESS':
-      return { ...state, currentProductLoading: false, data: action.payload };
+      return { ...state, data: action.payload };
     case 'FETCH_PRODUCT_FAILURE':
       return state;
     default:
