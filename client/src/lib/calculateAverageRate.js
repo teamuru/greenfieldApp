8b8c9ddceb49b98e52bd33ad09a9f8e4025d617b
Hongwei <qhwatdmm@hotmail.com>
2019-07-17 16:15:29 -0400

    change calculatorAverageRate to calculateAverageRate and delete dist file

--- client/src/lib/calculateAverageRate.js
+++ client/src/lib/calculateAverageRate.js
@@ -2,19 +2,19 @@
 // obj = { 2: 1, 3: 1, 4: 2, ... }
 // return average rate rounding to nearest 0.25
 
-function calculatorAverageRate(obj) {
+function calculateAverageRate(obj) {
   let rateSum = 0;
   let rateCounter = 0;
-  Object.keys(obj).forEach((k)=>{
+  Object.keys(obj).forEach((k) => {
     rateSum += Number(k) * obj[k];
-      rateCounter += obj[k];
-  })
+    rateCounter += obj[k];
+  });
 
   const average = rateSum / rateCounter;
   return Number(Math.round(average * 4) / 4);
 }
 
-export default calculatorAverageRate;
+export default calculateAverageRate;
 
 // test
 // var obj = {2: 1, 3: 1, 4: 2}
