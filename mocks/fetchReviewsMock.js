759ca49ab22bf52f378cbd540b038fd6a3998fbd
Cali Tabilas <ctabilas23@gmail.com>
2019-07-17 19:13:33 -0400

    created mock data for reviews test

---
+++ mocks/fetchReviewsMock.js
@@ -0,0 +1,18 @@
+export default {
+  type: 'FETCH_PRODUCT_SUCCESS',
+  payload: {
+    id: 1,
+    name: 'Camo Onesie',
+    slogan: 'Blend in to your crowd',
+    description:
+      'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+    category: 'Jackets',
+    default_price: '140',
+    features: [
+      {
+        feature: 'Buttons',
+        value: 'Brass'
+      }
+    ]
+  }
+};
