0243998548655914df2da6fa20adf6b4b6c1936d
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-18 10:14:24 -0400

    Added getRelatedProducts action

--- client/src/reducers/relatedReducer.js
+++ client/src/reducers/relatedReducer.js
@@ -6,6 +6,10 @@ const relatedReducer = (state = initialState, action) => {
       return { ...state, related: action.payload };
     case 'FETCH_RELATED_FAILURE':
       return state;
+    case 'FETCH_RELATED_PRODUCT_SUCCESS':
+      return { ...state, relatedProducts: action.payload };
+    case 'FETCH_RELATED_PRODUCT_FAILURE':
+      return state;
     default:
       return state;
   }
