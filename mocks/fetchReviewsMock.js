2e7cd71a4640da2cb7a14c558123371bcf857198
Cali Tabilas <ctabilas23@gmail.com>
2019-07-18 10:29:21 -0400

    api back up - seeing if test is running

--- mocks/fetchReviewsMock.js
+++ mocks/fetchReviewsMock.js
@@ -1,84 +1,32 @@
 export default {
-  product: '2',
+  product: '1',
   page: 0,
   count: 5,
   results: [
     {
-      review_id: 5,
-      rating: 3,
-      summary: "I'm enjoying wearing these shades",
-      recommend: 0,
-      response: '',
-      body: 'Comfortable and practical.',
-      date: '2019-04-14T00:00:00.000Z',
-      reviewer_name: 'shortandsweeet',
-      helpfulness: 5,
-      photos: [
-        {
-          id: 1,
-          url:
-            'https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80'
-        },
-        {
-          id: 2,
-          url:
-            'https://images.unsplash.com/photo-1561693532-9ff59442a7db?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
-        },
-        {
-          id: 3,
-          url:
-            'https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
-        }
-      ]
-    },
-    {
-      review_id: 3,
-      rating: 4,
-      summary: 'I am liking these glasses',
-      recommend: 0,
-      response: "Glad you're enjoying the product!",
-      body:
-        "They are very dark.  But that's good because I'm in very sunny spots",
-      date: '2019-06-23T00:00:00.000Z',
-      reviewer_name: 'bigbrotherbenjamin',
-      helpfulness: 5,
-      photos: []
-    },
-    {
-      review_id: 4,
-      rating: 4,
-      summary: 'They look good on me',
-      recommend: 0,
-      response: '',
-      body: 'I so stylish and just my aesthetic.',
-      date: '2019-03-12T00:00:00.000Z',
-      reviewer_name: 'fashionperson',
-      helpfulness: 1,
-      photos: []
-    },
-    {
-      review_id: 6,
+      review_id: 1,
       rating: 5,
-      summary: "I'm not a fan!",
+      summary: 'This product was great!',
       recommend: 0,
-      response:
-        "Sorry to hear. Is there anything in particular you don't like?",
-      body: "I don't like them",
-      date: '2019-06-16T00:00:00.000Z',
-      reviewer_name: 'negativity',
-      helpfulness: 0,
+      response: '',
+      body:
+        'I really did or did not like this product based on whether it was sustainably sourced.  Then I found out that its made from nothing at all.',
+      date: '2019-01-01T00:00:00.000Z',
+      reviewer_name: 'funtime',
+      helpfulness: 8,
       photos: []
     },
     {
-      review_id: 7,
-      rating: 2,
+      review_id: 2,
+      rating: 4,
       summary: 'This product was ok!',
       recommend: 0,
       response: '',
-      body: "They're fine but I wouldn't buy again.",
-      date: '2019-05-23T00:00:00.000Z',
-      reviewer_name: 'anyone',
-      helpfulness: 0,
+      body:
+        'I really did not like this product solely because I am tiny and do not fit into it.',
+      date: '2019-01-11T00:00:00.000Z',
+      reviewer_name: 'mymainstreammother',
+      helpfulness: 2,
       photos: []
     }
   ]
