aa1ab38bd5957389ea821f83f8573ec886c55c4c
Cali Tabilas <ctabilas23@gmail.com>
2019-07-18 10:32:11 -0400

    reviews not review

--- client/src/actions/reviewActions.js
+++ client/src/actions/reviewActions.js
@@ -1,19 +1,20 @@
 import Axios from 'axios';
 import API_URL from '../lib/API_URL';
 
-export const fetchReviewsSuccess = reviews => ({
-  type: 'FETCH_REVIEW_SUCCESS',
+export const fetchReviewsSuccess = (reviews) => ({
+  type: 'FETCH_REVIEWS_SUCCESS',
   payload: reviews
 });
 
-export const fetchReviewFailure = err => ({
-  type: 'FETCH_REVIEW_FAILURE',
+export const fetchReviewFailure = (err) => ({
+  type: 'FETCH_REVIEWS_FAILURE',
   payload: err
 });
 
 export const fetchReviews = (productId) => {
   const url = `${API_URL}/reviews/${productId}/list`;
-  return dispatch => Axios.get(url)
+  return (dispatch) =>
+    Axios.get(url)
       .then(({ data }) => {
         dispatch(fetchReviewsSuccess(data));
       })
