a63c6c2f0c72cc74deb6828dcb2022ed599f6435
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-19 11:30:00 -0400

    rebase complete

--- client/src/components/Related/ProductCard.jsx
+++ client/src/components/Related/ProductCard.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+// import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -38,5 +38,7 @@ const ProductCard = () => {
     </>
   );
 };
+
+// }
 
 export default ProductCard;
