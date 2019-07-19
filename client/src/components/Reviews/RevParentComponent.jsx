759fc62f2675df0a06059fa62b93ca62038ef2f3
Cali Tabilas <ctabilas23@gmail.com>
2019-07-18 23:51:19 -0400

    basic component rendering

--- client/src/components/Reviews/RevParentComponent.jsx
+++ client/src/components/Reviews/RevParentComponent.jsx
@@ -3,6 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
+// Children Components
+import MoreReviews from './MoreReviews';
+import AddReviewModal from './AddReviewModal.jsx';
+import Recommended from './Recommended.jsx'
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1
@@ -27,12 +32,15 @@ const RevParentComponent = () => {
       <Grid container spacing={3}>
         <Grid item xs>
           <Paper className={classes.paper}>Rating and Star</Paper>
-          <Paper className={classes.paper}>Recommendation</Paper>
+          <Paper className={classes.paper}>
+          <Recommended />
+          </Paper>
           <Paper className={classes.paper}>Stars List</Paper>
           <Paper className={classes.paper}>Slider</Paper>
         </Grid>
         <Grid item xs={9}>
           <Paper className={classes.paper}>Sort By Relevance</Paper>
+          {/* MAP HERE */}
           <Paper className={classes.paper}>
             <Grid>
               <Grid container justify="space-between" direction="row">
@@ -43,26 +51,34 @@ const RevParentComponent = () => {
                 <Grid item>Summary Bold</Grid>
                 <Grid item>Body</Grid>
                 <Grid item>Recommend Y/N</Grid>
-                <Grid container alignItems="baseline">
-                  <Grid item>Response</Grid>
+                <Grid
+                  container
+                  // alignItems="baseline"
+                >
+                  <Grid item xs={9}>
+                    Response
+                  </Grid>
                   <Grid item>Response Summary</Grid>
                 </Grid>
                 <Grid item>Helpful | yes?</Grid>
               </Grid>
-              <Grid>
-                <Grid
-                  container
-                  alignItems="baseline"
-                  spacing={6}
-                  justify="flex-start"
-                  direction="row"
-                >
-                  <Grid item>stars</Grid>
-                  <Grid item>User</Grid>
-                </Grid>
-              </Grid>
+              <Grid />
             </Grid>
           </Paper>
+          <Grid
+            container
+            alignItems="baseline"
+            spacing={6}
+            justify="flex-start"
+            direction="row"
+          >
+            <Grid item>
+              <MoreReviews />
+            </Grid>
+            <Grid item>
+              <AddReviewModal />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
