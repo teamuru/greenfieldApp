307ae28b9b6247e154d5015375d6a6abca0492e5
Cali Tabilas <ctabilas23@gmail.com>
2019-07-19 19:31:31 -0400

    added meta into the store

--- client/src/reducers/reviewsReducer.js
+++ client/src/reducers/reviewsReducer.js
@@ -3,17 +3,15 @@ const intialState = {};
 const reviewsReducer = (state = intialState, action) => {
   switch (action.type) {
     case 'FETCH_REVIEWS_SUCCESS':
-      return { ...state, data: action.payload };
+      return { ...state, reviews: action.payload };
     case 'FETCH_REVIEWS_FAILURE':
       return state;
-
-      case 'FETCH_META_SUCCESS':
-          return { ...state, data: action.payload };
-        case 'FETCH_META_FAILURE':
-          return state;
-
+    case 'FETCH_META_SUCCESS':
+      return { ...state, meta: action.payload };
+    case 'FETCH_META_FAILURE':
+      return state;
     case 'POST_REVIEWS_SUCCESS':
-      return { ...state, data: action.payload };
+      return { ...state, review: action.payload };
     case 'POST_REVIEWS_FAILURE':
       return state;
     default:
