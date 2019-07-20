224980b70e5ebf51e4657dc4a3e7581f220e9cf0
Cali Tabilas <ctabilas23@gmail.com>
2019-07-20 16:33:37 -0400

    everything is still rendering - working on normalize

--- client/src/components/Reviews/StarGraphsList.jsx
+++ client/src/components/Reviews/StarGraphsList.jsx
@@ -7,17 +7,30 @@ import StarGraph from './StarGraph.jsx';
 
 export class StarGraphsList extends Component {
   // some functtions here
+  handleNormalize(current, total) {
+    return (current / total) * 100;
+  }
+
   render() {
     const ratings = [1, 2, 3, 4, 5];
-    const { meta } = this.props;
-    return (
+    const { meta } = this.props.reviews;
+
+    return !meta ? (
+      <h1> ... Loading</h1>
+    ) : (
+      // const totalRatings = Object.values(meta.ratings);
+
       <React.Fragment>
         {ratings.map((element) => {
+          {
+            /* let normalize = this.handleNormalize() */
+          }
+
           return (
             <div style={{ fontSize: 15 }} key={element}>
               {element} Stars
               <StarGraph variant="determinate" value={element} />
-              {console.log(`this.props.reviews.meta`, this.props.reviews.meta)}
+              {console.log(`this.props.reviews.meta`, meta.ratings)}
             </div>
           );
         })}
