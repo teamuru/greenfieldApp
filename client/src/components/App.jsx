2c4f2b9f17968ae660200c2f5115dde26bcadcd7
Hongwei <qhwatdmm@hotmail.com>
2019-07-20 15:47:50 -0400

    add load more answer function 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,18 @@
 /* eslint no-shadow: "off" */
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Container from "@material-ui/core/Container";
-import theme from "../theme";
-import Related from "./Related/Related";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
+import theme from '../theme';
+import Related from './Related/Related';
 
 // component imports
-import ProductOverview from "./Product/ProductOverview";
-import Questions from "./Questions/Question";
-import ReviewsWrapper from "./Reviews/ReviewsWrapper";
+import ProductOverview from './Product/ProductOverview';
+import Questions from './Questions/Question';
+import ReviewsWrapper from './Reviews/ReviewsWrapper';
 
 class App extends Component {
   componentDidMount() {}
