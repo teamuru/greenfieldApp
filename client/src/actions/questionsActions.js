90ec4dac8a51084882c0fd94f62c05967c53c716
Hongwei <qhwatdmm@hotmail.com>
2019-07-22 19:29:26 -0400

    add helpfulness on question 1

--- client/src/actions/questionsActions.js
+++ client/src/actions/questionsActions.js
@@ -1,24 +1,23 @@
-import Axios from "axios";
-import API_URL from "../lib/API_URL";
+import Axios from 'axios';
+import API_URL from '../lib/API_URL';
 
 export const fetchQuestionsSuccess = questions => ({
-  type: "FETCH_QUESTIONS_SUCCESS",
+  type: 'FETCH_QUESTIONS_SUCCESS',
   payload: questions
 });
 
 export const fetchQuestionsError = error => ({
-  type: "FETCH_QUESTIONS_ERROR",
+  type: 'FETCH_QUESTIONS_ERROR',
   error
 });
 
-export const fetchQuestions = prodId => {
+export const fetchQuestions = (prodId) => {
   const url = `${API_URL}/qa/${prodId}`;
-  return dispatch =>
-    Axios.get(url)
+  return dispatch => Axios.get(url)
       .then(({ data }) => {
         dispatch(fetchQuestionsSuccess(data.results));
       })
-      .catch(err => {
+      .catch((err) => {
         dispatch(fetchQuestionsError(err));
       });
 };
@@ -38,22 +37,22 @@ export const postAddAnswer = (answer, name, email, photos, id) => {
     photos
   })
     .then(() => {
-      console.log("Success to post AddAnswer");
+      console.log('Success to post AddAnswer');
     })
     .catch(() => {
-      console.log("Fail to post AddAnswer");
+      console.log('Fail to post AddAnswer');
     });
 };
 
-export const pullHelpful = id => {
+export const pullHelpful = (id) => {
   const url = `${API_URL}/qa/question/${id}/helpful`;
-  console.log("id for pull: ", id);
+  console.log('id for pull: ', id);
   Axios.put(url, { question_id: id })
     .then(() => {
-      console.log("success pull helpful");
+      console.log('success pull helpful');
     })
     .catch(() => {
-      console.log("fail pull helpful");
+      console.log('fail pull helpful');
     });
 };
 
