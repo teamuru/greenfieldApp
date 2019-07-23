60d46f503f58d2b53c48722632885dfa4b8bf5e7
Hongwei <qhwatdmm@hotmail.com>
2019-07-23 11:26:31 -0400

    add mouse on and off on more answered question and add a question

--- client/src/components/Questions/AddQuestionModal.jsx
+++ client/src/components/Questions/AddQuestionModal.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
 class AddQuestionModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hover: "normal" };
+    this.handleHoverOn = this.handleHoverOn.bind(this);
+    this.handleHoverOff = this.handleHoverOff.bind(this);
+  }
+  handleHoverOn() {
+    this.setState({ hover: "bold" });
+  }
+  handleHoverOff() {
+    this.setState({ hover: "normal" });
+  }
   render() {
     return (
       <span>
-        <button style={{ fontWeight: "bold", fontSize: 14 }}>
+        <button
+          onMouseEnter={this.handleHoverOn}
+          onMouseLeave={this.handleHoverOff}
+          style={{ fontWeight: this.state.hover, fontSize: 14 }}
+        >
           ADD A QUESTION +
         </button>
       </span>
--- client/src/components/Questions/MoreAnsweredQuestions.jsx
+++ client/src/components/Questions/MoreAnsweredQuestions.jsx
@@ -3,8 +3,17 @@ import React, { Component } from "react";
 class MoreAnsweredQuestions extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 2 };
+    this.state = { count: 2, hover: "normal" };
     this.hangelClick = this.hangelClick.bind(this);
+    this.handleHoverOn = this.handleHoverOn.bind(this);
+    this.handleHoverOff = this.handleHoverOff.bind(this);
+  }
+
+  handleHoverOn() {
+    this.setState({ hover: "bold" });
+  }
+  handleHoverOff() {
+    this.setState({ hover: "normal" });
   }
 
   hangelClick() {
@@ -18,7 +27,9 @@ class MoreAnsweredQuestions extends Component {
     return (
       <span>
         <button
-          style={{ fontWeight: "bold", fontSize: 14 }}
+          onMouseEnter={this.handleHoverOn}
+          onMouseLeave={this.handleHoverOff}
+          style={{ fontWeight: this.state.hover, fontSize: 14 }}
           onClick={this.hangelClick}
         >
           MORE ANSWERED QUESTIONS
