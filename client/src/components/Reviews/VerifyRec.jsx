852eaea1c0bd7a3b624d6c3c427fbb2ac0ff69a4
Cali Tabilas <ctabilas23@gmail.com>
2019-07-23 17:16:22 -0400

    recommend this product

--- client/src/components/Reviews/VerifyRec.jsx
+++ client/src/components/Reviews/VerifyRec.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Grid, Box } from '@material-ui/core';
+import Check from '@material-ui/icons/CheckCircle';
 
 function VerifyRec({ review }) {
-  return (
+  return review.recommend === 0 ? (
+    <span> </span>
+  ) : (
     <div>
-      <h4>Users recommend this product</h4>
-      {review}
+      <Grid
+        container
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="center"
+        style={{ fontSize: 15, padding: '15px 0px' }}
+      >
+        <Check style={{ fontSize: 20 }} color="secondary" />
+
+        <span> {'   '} I recommend this product</span>
+      </Grid>
     </div>
   );
 }
