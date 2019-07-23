9c224e13e8c64f2d26afa46f4a608b7bf5eccab4
Hongwei <qhwatdmm@hotmail.com>
2019-07-23 17:12:57 -0400

    add search question func 2

--- client/src/reducers/questionsReducer.js
+++ client/src/reducers/questionsReducer.js
@@ -4,11 +4,11 @@ const initalState = {
 };
 const questionsReducer = (state = initalState, action) => {
   switch (action.type) {
-    case "FETCH_QUESTIONS_SUCCESS":
+    case 'FETCH_QUESTIONS_SUCCESS':
       return { ...state, data: action.payload, displayList: action.payload };
-    case "FETCH_QUESTIONS_ERROR":
+    case 'FETCH_QUESTIONS_ERROR':
       return { ...state, error: action.error };
-    case "DISPLAY_QUESTIONS":
+    case 'DISPLAY_QUESTIONS':
       return { ...state, displayList: action.payload };
     default:
       return state;
