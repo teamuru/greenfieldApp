56ad231c4e5e6d2c77539056ea843cd8f7c247b8
Cali Tabilas <ctabilas23@gmail.com>
2019-07-25 15:58:32 -0400

    revised actions

--- client/src/actions/reviewsActions.js
+++ client/src/actions/reviewsActions.js
@@ -2,45 +2,43 @@ import Axios from 'axios';
 import API_URL from '../lib/API_URL';
 
 // GET REVIEWS
-export const fetchReviewsSuccess = (reviews) => ({
+export const fetchReviewsSuccess = reviews => ({
   type: 'FETCH_REVIEWS_SUCCESS',
   payload: reviews
 });
 
-export const fetchReviewsFailure = (error) => ({
+export const fetchReviewsFailure = error => ({
   type: 'FETCH_REVIEWS_FAILURE',
   payload: error
 });
 
 export const fetchReviews = (prodId) => {
   const url = `${API_URL}/reviews/${prodId}/list`;
-  return (dispatch) =>
-    Axios.get(url)
+  return dispatch => Axios.get(url)
       .then(({ data }) => {
         dispatch(fetchReviewsSuccess(data));
       })
-      .catch((err) => dispatch(fetchReviewsFailure(err)));
+      .catch(err => dispatch(fetchReviewsFailure(err)));
 };
 
 // GET META
-export const fetchMetaSuccess = (meta) => ({
+export const fetchMetaSuccess = meta => ({
   type: 'FETCH_META_SUCCESS',
   payload: meta
 });
 
-export const fetchMetaFailure = (error) => ({
+export const fetchMetaFailure = error => ({
   type: 'FETCH_META_FAILURE',
   payload: error
 });
 
 export const fetchMeta = (prodId) => {
   const url = `${API_URL}/reviews/${prodId}/meta`;
-  return (dispatch) =>
-    Axios.get(url)
+  return dispatch => Axios.get(url)
       .then(({ data }) => {
         dispatch(fetchMetaSuccess(data));
       })
-      .catch((err) => dispatch(fetchMetaFailure(err)));
+      .catch(err => dispatch(fetchMetaFailure(err)));
 };
 
 // Helpful
@@ -71,20 +69,19 @@ export const reportReview = (reviewId) => {
 };
 
 // Post from Form
-export const postReviewSucess = (review) => ({
+export const postReviewSucess = review => ({
   type: 'POST_REVIEW_SUCCESS',
   payload: review
 });
 
-export const postReviewFailure = (error) => ({
+export const postReviewFailure = error => ({
   type: 'POST_REVIEW_FAILURE',
   payload: error
 });
 
 export const postReview = (reviewObj, prodId) => {
   const url = `${API_URL}/reviews/${prodId}`;
-  return (dispatch) =>
-    Axios.post(url, reviewObj)
+  return dispatch => Axios.post(url, reviewObj)
       .then(({ data }) => {
         dispatch(postReviewSucess(data));
         dispatch(fetchReviews(prodId));
